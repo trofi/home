a08d2fd8d1a014a04b5715d4a39150bfa219d08a
Sergei Trofimovich <slyfox@gentoo.org>
2020-07-15 18:49:17 +0100

    xmonad.hs: restore alsamixer default, rely on dmenu for backup terminal
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -45,8 +45,8 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
     -- launch a terminal
     [ ((controlMask ,          xK_grave ), spawnx (myTerminal ++ " -e tmux"))
-    , ((mod4Mask    ,          xK_grave ), spawnx myTerminal)
-    , ((modMask     ,          xK_grave ), spawnx "xterm" )
+    , ((mod4Mask    ,          xK_grave ), spawnx (myTerminal ++ " -e alsamixer"))
+    , ((modMask     ,          xK_grave ), spawnx myTerminal)
 
     -- launch dmenu
     , ((mod4Mask,               xK_p     ), spawnx "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")

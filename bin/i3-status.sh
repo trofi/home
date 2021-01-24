9e24fac0a72012d503185214ead9ddca535087f9
Sergei Trofimovich <slyfox@gentoo.org>
2021-01-24 09:38:06 +0000

    i3-status: add NIC usage stats entry
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- bin/i3-status.sh
+++ bin/i3-status.sh
@@ -3,12 +3,21 @@
 # it's an i3status wrapper with external tool that populate
 # status updates:
 # - xmms2 status
+# - NIC interface usage
 
 (
     xmms2_status_file=/tmp/i3status-xmms2
     printf "<span>hello!</span>\n" > "${xmms2_status_file}"
     $HOME/bin/player-status-entry-persist.py pango | while read l; do
         printf "%s\n" "${l}" > "${xmms2_status_file}"
+    done
+) &
+
+(
+    nic_status_file=/tmp/i3status-nic-usage
+    printf "<span>hello!</span>\n" > "${nic_status_file}"
+    $HOME/bin/nic-status-persist.bash auto | while read rx tx; do
+        printf "<span>rx:${rx} / tx:${tx}</span>\n" > "${nic_status_file}"
     done
 ) &
 
---
+++ bin/nic-status-persist.bash
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# seconds
+poll_interval=4
+interface=$1
+
+if [[ -z ${interface} ]]; then
+    echo "ERROR: no interface specified"
+    echo
+    echo "usage: $0 <interface>"
+    echo "  Available /sys/class/net interfaces: "
+    cd /sys/class/net
+    printf "    - %s\n" *
+    exit 1
+fi
+
+# pick first wlan-looking interface
+if [[ ${interface} == auto ]]; then
+    cd /sys/class/net
+    set -- w*
+    interface="$1"
+fi
+
+get_if_metric() {
+    local metric=$1
+    cat "/sys/class/net/${interface}/statistics/${metric}"
+}
+
+get_rx_bytes() {
+    get_if_metric rx_bytes
+}
+
+get_tx_bytes() {
+    get_if_metric tx_bytes
+}
+
+pp_bytes() {
+    local bytes=$1
+    numfmt --to=iec-i --suffix=B "${bytes}"
+}
+
+rx_prev=$(get_rx_bytes)
+tx_prev=$(get_tx_bytes)
+
+while :
+do
+    rx=$(get_rx_bytes)
+    tx=$(get_tx_bytes)
+
+    echo $(pp_bytes $(( ( rx - rx_prev ) / poll_interval ))) $(pp_bytes $(( ( tx - tx_prev ) / poll_interval )))
+
+    rc_prev=${rx}
+    tx_prev=${tx}
+    sleep "${poll_interval}"
+done

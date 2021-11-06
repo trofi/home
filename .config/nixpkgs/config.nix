762bb020583370e28b1a8eabc5d88a5692e51bdb
Sergei Trofimovich <slyich@gmail.com>
2021-11-06 19:16:39 +0000

    .config/nixpkgs/config.nix: add NUR template example

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,4 +1,11 @@
 {
   # Too scared to try on a regular basis :)
   # contentAddressedByDefault = true;
+
+  # NUR!
+  # packageOverrides = pkgs: {
+  #   nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+  #     inherit pkgs;
+  #   };
+  # };
 }

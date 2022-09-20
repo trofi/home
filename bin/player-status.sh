b510ed996ee29cbfdd93fa52b11509e283401918
Sergei Trofimovich <slyich@gmail.com>
2022-09-20 07:51:56 +0100

    ~/bin/player-status.sh: fix pango markup in error message

--- bin/player-status.sh
+++ bin/player-status.sh
@@ -2,6 +2,6 @@
 
 while :
 do
-    $HOME/bin/player-status-entry-persist.py pango 2>&1 || echo "<fc=red>[error]</fc>"
+    $HOME/bin/player-status-entry-persist.py pango 2>&1 || echo "<span foreground='red'>[error]</span>"
     sleep 4
 done

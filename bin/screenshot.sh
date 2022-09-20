0c5e5606a0e54a4ddb8433fb5321c8a65ab2cdd0
Sergei Trofimovich <slyich@gmail.com>
2022-09-20 23:34:52 +0100

    ~/bin/screenshot.sh: updated for nix and wayland

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,6 +1,20 @@
-#!/bin/bash
+#! /usr/bin/env nix-shell
+#! nix-shell --pure
+#! nix-shell -p alsa-utils
+#! nix-shell -p grim -p imagemagick
+#! nix-shell -i bash
 
-DIR=~/archive/shots
+set -e
+
+cd ~/archive/shots
 FILE="$(date "+%F-%H-%M-%S").png"
-import -window root "$DIR/$FILE"
+
+if [[ -n $WAYLAND_DISPLAY ]]; then
+    grim "$FILE"
+elif [[ -n $DISPLAY ]]; then
+    import -window root "$FILE"
+else
+    echo "ERROR: how should I shot?"
+    exit 1
+fi
 aplay ~/bin/done.wav

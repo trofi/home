4ca0e28e7ac6264bd6d12568738475789406798c
Sergei Trofimovich <slyich@gmail.com>
2022-10-02 10:07:23 +0100

    .rc.d/wayland/sway/run.sh: pull in NixOS generated fonts

--- .rc.d/wayland/sway/run.sh
+++ .rc.d/wayland/sway/run.sh
@@ -12,4 +12,13 @@ dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 gsettings set org.gnome.desktop.interface cursor-theme Adwaita
 gsettings set org.gnome.desktop.interface cursor-size 32
 
+for f in /usr/share/fonts/* /run/current-system/sw/share/X11/fonts/
+do
+    if [[ -d $f ]]; then
+      echo "adding fontdir '$f'"
+      xset +fp "$f"
+    fi
+done
+xset fp rehash
+
 xrdb -merge "$HOME/.Xresources"
